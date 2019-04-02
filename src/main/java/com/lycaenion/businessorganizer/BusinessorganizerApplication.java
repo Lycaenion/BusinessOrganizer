c096944cad10061e389fcbee6886913ebc906ba5
Lycaenion <linn.martensson@hotmail.se>
2019-04-02 20:25:25 +0200

    created spring project

---
+++ src/main/java/com/lycaenion/businessorganizer/BusinessorganizerApplication.java
@@ -0,0 +1,13 @@
+package com.lycaenion.businessorganizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessorganizerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BusinessorganizerApplication.class, args);
+    }
+
+}
