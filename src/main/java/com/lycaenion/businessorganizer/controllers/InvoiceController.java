f3284e5ebe045cd529cfde9bef1763e9564cf8de
Lycaenion <linn.martensson@hotmail.se>
2019-04-10 11:26:30 +0200

    added endpoints for invoices

--- src/main/java/com/lycaenion/businessorganizer/controllers/InvoiceController.java
+++ src/main/java/com/lycaenion/businessorganizer/controllers/InvoiceController.java
@@ -1,5 +1,8 @@
 package com.lycaenion.businessorganizer.controllers;
 
+import com.lycaenion.businessorganizer.entities.Invoice;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,9 +10,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/invoices")
 public class InvoiceController {
 
-    @RequestMapping("/greeting")
-    private String greeting(){
-        return "Hello from invoice";
+    @RequestMapping("/addInvoice")
+    private ResponseEntity<String> addInvoice(@RequestBody Invoice invoice){
+        return null;
+    }
+
+    @RequestMapping("/getInvoiceById")
+    private ResponseEntity<Invoice> getInvoice(@RequestBody int id){
+        return null;
+    }
+
+    @RequestMapping("/changeToPaid")
+    private ResponseEntity<String> changeInvoiceToPaid(@RequestBody int id){
+        return null;
     }
 
 }
