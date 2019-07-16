2690e2f123be84d880d509d0d0065404571980fb
Lycaenion <linn.martensson@hotmail.se>
2019-07-16 21:22:45 +0200

    added methods to CustomerService interface

--- src/main/java/com/lycaenion/businessorganizer/services/CustomerService.java
+++ src/main/java/com/lycaenion/businessorganizer/services/CustomerService.java
@@ -1,4 +1,13 @@
 package com.lycaenion.businessorganizer.services;
 
+import com.lycaenion.businessorganizer.dto.CustomerRequest;
+import com.lycaenion.businessorganizer.entities.Customer;
+
 public interface CustomerService {
+
+    String saveCustomer(Customer customer);
+
+    Customer editCustomer(Customer customer);
+
+    Customer fetchCustomerByNameOrId(CustomerRequest customerRequest);
 }
